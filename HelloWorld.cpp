63647bd4afb4ab3d3185f69d15d962e9cc580c65
John <johnleonardo207@gmail.com>
2025-06-23 13:21:40 -0400

    static hello

---
+++ HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include "HelloWorld.h"
+
+void HelloWorld::Print()
+{
+	std::cout << "Hello World Team3";
+}
---
+++ HelloWorld.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+class HelloWorld
+{
+public:
+	HelloWorld() {};
+
+	static void Print();
+};
+
